6d259645b0f9bf9742c735f4235443448689a54a
unknown <nhatnguyen.nguyen@torontomu.ca>
2025-06-27 18:15:55 -0400

    added images for collections and services

--- peridot-project/src/pages/Home.jsx
+++ peridot-project/src/pages/Home.jsx
@@ -1,6 +1,12 @@
-import p1 from'../assets/placeholder/p1.jpg'
-import p2 from'../assets/placeholder/p2.jpg'
-import p3 from'../assets/placeholder/p3.jpg'
+import p1 from'../assets/nails/1e4563fa-7156-4f44-bde3-d60bce4436cd.jpg'
+import p2 from'../assets/nails/614d77bc-f739-47e4-a905-092ec074af89.jpg'
+import p3 from'../assets/nails/37245208-ae1d-42e4-bad6-6810e0324d01.jpg'
+
+import p4 from'../assets/nails/226af816-01d9-444a-ba11-a51c2b120aad.jpg'
+import p5 from'../assets/nails/ac9ca8db-611b-4af2-9d74-bfc70bccd656.jpg'
+import p6 from'../assets/nails/37245208-ae1d-42e4-bad6-6810e0324d01.jpg'
+
+
 import {useEffect} from 'react'
 import ImageCarousel from '../components/ImageCarousel.jsx'
 import ServiceCard from '../components/ServiceCard.jsx'
@@ -34,8 +40,8 @@ function Home(){
             <div className="card-container">
                 <div className="card">
                     <Link to="/services"><ServiceCard image={p1} name="Pedicure"/></Link>
-                    <Link to="/services"><ServiceCard image={p2} name="Manicure"/></Link>
-                    <Link to="/services"><ServiceCard image={p3} name="Kids 11 and Under"/></Link>
+                    <Link to="/services"><ServiceCard image={p4} name="Manicure"/></Link>
+                    <Link to="/services"><ServiceCard image={p5} name="Kids 11 and Under"/></Link>
                 </div>
                 <div className="button-container">
                     <Link to="/services">
