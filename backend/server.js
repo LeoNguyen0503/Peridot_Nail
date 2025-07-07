3bfbcdd6d7e82f5ddea52479868188f68cf890c5
unknown <nhatnguyen.nguyen@torontomu.ca>
2025-07-06 23:57:06 -0400

    added db.js and models

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import {connectDB} from './config/db.js';
 
 dotenv.config();
 
@@ -9,9 +10,10 @@ app.get("/", (req, res) => {
     res.send("Server is running!");
 })
 
-console.log(process.env.MONGO_URI);
+
 
 app.listen(5000, () => {
+    connectDB();
     console.log("Server is running on http://locWalhost:5000");
 });
 
