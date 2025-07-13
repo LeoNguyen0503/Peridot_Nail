4b2c9896b44b6a643dc0a3fcebb7a0a043eb368c
unknown <nhatnguyen.nguyen@torontomu.ca>
2025-07-13 13:50:27 -0400

    added functions

--- peridot-project/src/pages/Booking.jsx
+++ peridot-project/src/pages/Booking.jsx
@@ -2,12 +2,6 @@ import {useEffect, useState} from "react";
 import EmployeeCard from "../components/EmployeeCard.jsx";
 import pic1 from '../assets/logo.png';
 
-
-
-
-
-
-
 function Booking(){
 
     const [employees, setEmployees] = useState([])
@@ -17,7 +11,12 @@ function Booking(){
         const response = await fetch("/api/employees");
 
         const employee = await response.json();
-        setEmployees(employee.data)
+
+        if (employee.success){
+            setEmployees(employee.data);
+        } else {
+            console.log(employee.message);
+        }
     }
 
 
@@ -28,13 +27,41 @@ function Booking(){
 
     console.log(employees);
 
+    const shortDayOfWeek = (days) =>{
+        const dayMap = {
+            "Monday": "Mon",
+            "Tuesday": "Tue",
+            "Wednesday": "Wed",
+            "Thursday": "Thu",
+            "Friday": "Fri",
+            "Saturday": "Sat",
+            "Sunday": "Sun"
+        }
+
+        let dayString = "";
+
+        for (let i = 0; i < days.length; i++){
+            dayString += dayMap[days[i]];
+            if (i < days.length -1){
+                dayString += ", ";
+            }
+        }
+
+        return dayString;
+    }
+
+    const firstLetterUppercase = (str) => {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
 
     return (
         <div className="booking-container">
             <ul>
                 {employees.map((employee, index) => (
                     <li key={index}>
-                        <EmployeeCard image={pic1} name={employee.name} availability={employee.availability} position={employee.position}/>
+                        <EmployeeCard image={pic1} name={employee.name.toUpperCase()}
+                                      availability={shortDayOfWeek(employee.availability)}
+                                      position={firstLetterUppercase(employee.position)}/>
                     </li>
                 ))}
             </ul>
